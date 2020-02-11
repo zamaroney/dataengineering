f9a567d13e6aaef1cd56cc0b04d763075637c517
Zachary Maroney <zamaroney7986@eagle.fgcu.edu>
2020-02-11 16:02:07 -0500

    Added json file and AuthorParse

--- src/main/java/com/github/zamaroney/Main.java
+++ src/main/java/com/github/zamaroney/Main.java
@@ -1,13 +1,24 @@
 package com.github.zamaroney;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import com.opencsv.exceptions.CsvValidationException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException, CsvValidationException{
+  public static void main(String[] args) throws IOException, CsvValidationException {
 
     CsvParse csvP = new CsvParse("src/data/SEOExample-1.csv");
     csvP.printCsv();
+
+    Gson gson = new Gson();
+    JsonReader jread = new JsonReader(new FileReader("src/Data/authors.json"));
+    AuthorParse[] authors = gson.fromJson(jread, AuthorParse[].class);
+
+    for (var element : authors) {
+      System.out.println(element.getName());
+    }
   }
 }
